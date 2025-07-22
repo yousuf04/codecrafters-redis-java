152f9e607ce051bed84ee5d67a7af0c75dacd6e4
yousuf04 <yousuf2004.sms@gmail.com>
2025-07-23 00:58:22 +0530

    Responding to messages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -19,9 +20,12 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
-          System.out.println("Server Port is : "+serverSocket.getLocalPort()+"Client Port is "+clientSocket.getLocalPort());
-            OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write("+PONG\r\n".getBytes());
+          while(true) {
+              InputStream inputStream = clientSocket.getInputStream();
+              inputStream.read();
+              OutputStream outputStream = clientSocket.getOutputStream();
+              outputStream.write("+PONG\r\n".getBytes());
+          }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
